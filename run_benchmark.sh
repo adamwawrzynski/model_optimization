97534f0291fe03e84a5f6c623bb00a1567b8c346
Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>
2023-02-22 11:55:10 +0100

    Fix benchmark command argument
    Signed-off-by: Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -13,7 +13,7 @@ for BATCH_SIZE in "1" "16" "32" "64"; do
     poetry run python3 benchmark.py --type tensorrt --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
     poetry run python3 benchmark.py --type tensorrt --use_fp16 --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
     poetry run python3 benchmark.py --type tensorrt --use_jit --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
-    poetry run python3 benchmark.py --type tensorrt --use_use_fp16fp --use_jit --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
+    poetry run python3 benchmark.py --type tensorrt --use_fp16 --use_jit --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
     poetry run python3 benchmark.py --type quantization --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
     poetry run python3 benchmark.py --type dynamic_quantization --batch_size $BATCH_SIZE --n_runs $N_RUNS --model_name $MODEL_NAME
 done
